df938337f92230e1b954f4aa65a09ef813f1dd35
jeremyfelt <jeremy.felt@gmail.com>
2014-12-03 15:02:46 -0800

    Review and clean search.php template

--- search.php
+++ search.php
@@ -4,7 +4,7 @@ get_header();
 
 	<div id="content2" class="narrowcolumn">
 
-	<?php if (have_posts()) : ?>
+	<?php if ( have_posts() ) : ?>
 
 		<h3 class="pagetitle">Search Results</h3>
 
@@ -13,27 +13,25 @@ get_header();
 			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
 		</div>
 
-
-		<?php while (have_posts()) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
 			<div class="post">
 				<h1 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 				<small><?php the_time('l, F jS, Y') ?></small>
-
-				<p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
+				<p class="postmetadata"><?php the_tags( 'Tags: ', ', ', '<br />' ); ?> Posted in <?php the_category( ', ' ) ?> | <?php edit_post_link( 'Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;' ); ?></p>
 			</div>
 
 		<?php endwhile; ?>
 
 		<div class="navigation">
-			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+			<div class="alignleft"><?php next_posts_link( '&laquo; Older Entries' ); ?></div>
+			<div class="alignright"><?php previous_posts_link( 'Newer Entries &raquo;' ); ?></div>
 		</div>
 
 	<?php else : ?>
 
 		<h2 class="center">No posts found. Try a different search?</h2>
-		<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+		<?php get_search_form(); ?>
 
 	<?php endif; ?>
 
