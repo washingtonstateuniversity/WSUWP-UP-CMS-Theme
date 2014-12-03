d45237de18474889fc77b7c1d271e198c2c17fe2
jeremyfelt <jeremy.felt@gmail.com>
2014-12-03 12:17:06 -0800

    Register the theme's sidebar on widgets_init

--- functions.php
+++ functions.php
@@ -1,8 +1,15 @@
 <?php
-if ( function_exists('register_sidebar') )
-    register_sidebar(array(
-        'before_widget' => '<p id="%1$s" class="widget %2$s">',
-        'after_widget' => '</p>',
-        'before_title' => '<h4 class="widgettitle">',
-        'after_title' => '</h4>',
-    ));
+
+add_action( 'widgets_init', 'upcms_theme_widgets_init' );
+/**
+ * Register sidebars used by the theme.
+ */
+function upcms_theme_widgets_init() {
+	$widget_options = array(
+		'before_widget' => '<p id="%1$s" class="widget %2$s">',
+		'after_widget' => '</p>',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	);
+	register_sidebar( $widget_options );
+}
