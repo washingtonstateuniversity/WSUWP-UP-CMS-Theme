fe931e1999c607b219d3984bfddc842dbe4c9d1b
jeremyfelt <jeremy.felt@gmail.com>
2014-12-03 15:04:03 -0800

    Review searchform.php template

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
+<form method="get" id="searchform" action="<?php echo esc_url( home_url() ); ?>">
 <label class="hidden" for="s"><?php _e('Search for:'); ?></label>
 <div><input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
 <div style="padding-bottom:3px;"></div><input type="submit" id="searchsubmit" value="Search" />
