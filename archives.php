42911333bbb65632e15fadf2266dc6add178081f
NateOwen <idahoduckhunter@gmail.com>
2015-02-13 11:53:57 -0800

    Fix archives

--- archives.php
+++ archives.php
@@ -5,24 +5,23 @@ Template Name: Archives
 $cms_template_args = array( 'url' => get_the_permalink() );
 upcms_display_template_before( $cms_template_args );
 ?>
-<style>input.form-white { width:95% !important; }</style>
-<h3>Archives</h3>
-
 
 <aside>
 	<?php get_sidebar();?>
 </aside>
+<h3>List of Perspectives</h3>
 
-<ul>
+
+
 <?php
 
 $myposts = get_posts( array( 'posts_per_page' => 2000 ) );
 foreach( $myposts as $post ) {
-	?><li><?php the_time( 'm/d/y' ) ?>: <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li><?php
+	?><p><?php the_time( 'm/d/y' ) ?>: <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p><?php
 }
 
 ?>
-</ul>
+
 
 <?php
 get_sidebar();
