110459812c094c07e7de6f7407ad95cbf8d4edd2
NateOwen <idahoduckhunter@gmail.com>
2015-02-12 16:21:42 -0800

    sidebar fixes take 53

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,3 @@
-<div class="right">
-	<a href="http://feeds.feedburner.com/PresidentPerspectives"><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/ico-rss.png' ); ?>" alt="RSS feed" /></a>
-</div>
 <div class="sidebar theme1">
 	<nav>
 		<?php dynamic_sidebar(); ?>
